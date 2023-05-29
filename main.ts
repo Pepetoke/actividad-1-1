4e28b54232c3b216e03eec5d81c8f94c194dd1b7
Pepetoke <134915347+Pepetoke@users.noreply.github.com>
2023-05-29 09:04:12 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Marin")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Haro")
+})
+basic.showString("Jose Antonio")
+basic.forever(function () {
+	
+})
